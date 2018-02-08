54af8825bd2863b1a4463f09fd6ae8375be4dee3
marco <ma.buracchi@gmail.com>
2018-02-08 17:10:47 +0100

    Istogramma e standardizzazione

--- MASL_PCA/PCA.py
+++ MASL_PCA/PCA.py
@@ -4,10 +4,11 @@ Created on 08 feb 2018
 @author: marco
 '''
 
-if __name__ == '__main__':
-    pass
-
 import pandas as pd
+import numpy as np
+import math
+from matplotlib import pyplot as plt
+from sklearn.preprocessing import StandardScaler
 
 # download dataset
 df = pd.read_csv(
@@ -22,3 +23,41 @@ df.dropna(how="all", inplace=True) # Elimina i valori NA
 df.tail()
 
 print(df)
+
+# X = tabella con valori, y = etichette
+X = df.ix[:,0:4].values
+y = df.ix[:,4].values
+
+print(X)
+print(y)
+
+# creazione istogrammi
+label_dict = {1: 'Iris-Setosa',
+              2: 'Iris-Versicolor',
+              3: 'Iris-Virgnica'}
+
+feature_dict = {0: 'sepal length [cm]',
+                1: 'sepal width [cm]',
+                2: 'petal length [cm]',
+                3: 'petal width [cm]'}
+
+with plt.style.context('seaborn-whitegrid'):
+    plt.figure(figsize=(8, 6))
+    for cnt in range(4):
+        plt.subplot(2, 2, cnt+1)
+        for lab in ('Iris-setosa', 'Iris-versicolor', 'Iris-virginica'):
+            plt.hist(X[y==lab, cnt],
+                     label=lab,
+                     bins=10,
+                     alpha=0.3,)
+        plt.xlabel(feature_dict[cnt])
+    plt.legend(loc='upper right', fancybox=True, fontsize=8)
+
+    plt.tight_layout()
+    plt.show()
+    
+# normalizzazione    
+X_std = StandardScaler().fit_transform(X)
+
+if __name__ == '__main__':
+    pass
