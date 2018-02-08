bc4a0c2c0e98db2f2028b9188e5377c8dd76b08e
marco <ma.buracchi@gmail.com>
2018-02-08 16:49:37 +0100

    Aggiornato gitIgnore

---
+++ MASL_PCA/PCA.py
@@ -0,0 +1,22 @@
+'''
+Created on 08 feb 2018
+
+@author: marco
+'''
+
+if __name__ == '__main__':
+    pass
+
+import pandas as pd
+
+df = pd.read_csv(
+    filepath_or_buffer='https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data',
+    header=None,
+    sep=',')
+
+df.columns=['sepal_len', 'sepal_wid', 'petal_len', 'petal_wid', 'class']
+df.dropna(how="all", inplace=True) # drops the empty line at file-end
+
+df.tail()
+
+print(df)
