6db16869ae2da78de5de66af997bba09e78e9244
marco <ma.buracchi@gmail.com>
2018-02-08 16:54:10 +0100

    Inizio lavoro

--- MASL_PCA/PCA.py
+++ MASL_PCA/PCA.py
@@ -9,13 +9,15 @@ if __name__ == '__main__':
 
 import pandas as pd
 
+# download dataset
 df = pd.read_csv(
     filepath_or_buffer='https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data',
     header=None,
     sep=',')
 
-df.columns=['sepal_len', 'sepal_wid', 'petal_len', 'petal_wid', 'class']
-df.dropna(how="all", inplace=True) # drops the empty line at file-end
+# scelgo solamente le colonne con i valori di interesse
+df.columns=['sepal_len', 'sepal_wid', 'petal_len', 'petal_wid', 'class'] 
+df.dropna(how="all", inplace=True) # Elimina i valori NA
 
 df.tail()
 
