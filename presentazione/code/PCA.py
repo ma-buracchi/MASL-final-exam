5c9858c71512f27539a7be8f411249211b97fbc6
marco <ma.buracchi@gmail.com>
2018-02-18 12:50:17 +0100

    Cambio tema presentazione e prime slides

---
+++ presentazione/code/PCA.py
@@ -0,0 +1,171 @@
+'''
+Created on 08 feb 2018
+
+@author: marco
+'''
+
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA as sklearnPCA
+
+def separate():
+    print('\n############################################################\n')
+
+# download dataset
+df = pd.read_csv(
+    filepath_or_buffer='https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data',
+    header=None,
+    sep=',')
+
+# scelgo solamente le colonne con i valori di interesse
+df.columns=['sepal_len', 'sepal_wid', 'petal_len', 'petal_wid', 'class'] 
+df.dropna(how="all", inplace=True) # Elimina i valori NA
+print(df.tail()) #visualizza ultime 5 righe
+separate()
+
+# print(df)
+
+# X = tabella con valori, y = etichette
+X = df.ix[:,0:4].values
+y = df.ix[:,4].values
+
+# print(X)
+# print(y)
+
+# creazione istogrammi 
+feature_dict = {0: 'sepal length [cm]',
+                1: 'sepal width [cm]',
+                2: 'petal length [cm]',
+                3: 'petal width [cm]'}
+ 
+with plt.style.context('seaborn-whitegrid'):
+    plt.figure(figsize=(8, 6))
+    for cnt in range(4):
+        plt.subplot(2, 2, cnt+1)
+        for lab in ('Iris-setosa', 'Iris-versicolor', 'Iris-virginica'):
+            plt.hist(X[y==lab, cnt],
+                     label=lab,
+                     bins=10,
+                     alpha=0.3,)
+        plt.xlabel(feature_dict[cnt])
+    plt.legend(loc='upper right', fancybox=True, fontsize=8)
+ 
+    plt.tight_layout()
+    plt.show()
+    
+# normalizzazione dati 
+X_std = StandardScaler().fit_transform(X)
+
+# vettore delle medie e matrice di covarianza
+mean_vec = np.mean(X_std, axis=0)
+cov_mat = (X_std - mean_vec).T.dot((X_std - mean_vec)) / (X_std.shape[0]-1)
+print('Matrice di covarianza calcolata: \n%s\n' %cov_mat)
+
+# funzione di libreria
+print('Matrice di covarianza NumPy: \n%s\n' %np.cov(X_std.T))
+separate()
+
+# calcolo autovalori e autovettori su matrice di covarianza
+cov_mat = np.cov(X_std.T)
+eig_vals, eig_vecs = np.linalg.eig(cov_mat)
+print('Autovettori cov: \n%s\n' %eig_vecs)
+print('Autovalori cov: \n%s\n' %eig_vals)
+separate()
+
+# calcolo autovalori ed autovettori su matrice di correlazione dati standardizzati
+cor_mat1 = np.corrcoef(X_std.T)
+eig_vals, eig_vecs = np.linalg.eig(cor_mat1)
+print('Autovettori corrSTD: \n%s\n' %eig_vecs)
+print('Autovalori corrSTD: \n%s\n' %eig_vals)
+separate()
+
+# calcolo autovalori ed autovettori su matrice di correlazione dati grezzi
+cor_mat2 = np.corrcoef(X.T)
+eig_vals, eig_vecs = np.linalg.eig(cor_mat2)
+print('Autovettori corr: \n%s\n' %eig_vecs)
+print('Autovalori corr: \n%s\n' %eig_vals)
+separate()
+
+# decomposizione ai valori singolari
+u,s,v = np.linalg.svd(X_std.T)
+print('Autovettori SVD: \n%s\n' %u)
+separate()
+
+# ordinamento degli autovalori
+
+## creazione coppie (autovalore,autovettore)
+eig_pairs = [(np.abs(eig_vals[i]), eig_vecs[:,i]) for i in range(len(eig_vals))]
+
+## Ordinamento dal maggiore al minore
+eig_pairs.sort(key=lambda x: x[0], reverse=True)
+print('Autovalori ordinati:')
+for i in eig_pairs:
+    print(i[0])
+separate()
+
+# varianza spiegata
+tot = sum(eig_vals)
+var_exp = [(i / tot)*100 for i in sorted(eig_vals, reverse=True)]
+cum_var_exp = np.cumsum(var_exp)
+with plt.style.context('seaborn-whitegrid'):
+    plt.figure(figsize=(6, 4))
+
+    plt.bar(range(4), var_exp, alpha=0.5, align='center',
+            label='varianza spiegata individuale')
+    plt.step(range(4), cum_var_exp, where='mid',
+             label='varianza spiegata cumulata')
+    plt.ylabel('Rapporto varianza spiegata')
+    plt.xlabel('Componenti principali')
+    plt.legend(loc='best')
+    plt.tight_layout()
+    plt.show()
+
+# matrice di proiezione
+matrix_w = np.hstack((eig_pairs[0][1].reshape(4,1),
+                      eig_pairs[1][1].reshape(4,1)))
+
+print('Matrice W:\n', matrix_w)
+separate()
+
+# proiezione nel nuovo spazio
+Y = X_std.dot(matrix_w)
+print(Y)
+with plt.style.context('seaborn-whitegrid'):
+    plt.figure(figsize=(6, 4))
+    for lab, col in zip(('Iris-setosa', 'Iris-versicolor', 'Iris-virginica'),
+                        ('blue', 'red', 'green')):
+        plt.scatter(Y[y==lab, 0],
+                    Y[y==lab, 1],
+                    label=lab,
+                    c=col)
+    plt.xlabel('Componente Principale 1')
+    plt.ylabel('Componente Principale 2')
+    plt.legend(loc='lower center')
+    plt.tight_layout()
+    plt.show()
+    
+# pacchetto scikit-learn
+sklearn_pca = sklearnPCA(n_components=2)
+Y_sklearn = sklearn_pca.fit_transform(X_std)
+print(Y_sklearn)
+for i in Y_sklearn:
+    i[1] = -1*i[1]
+print(Y_sklearn)
+with plt.style.context('seaborn-whitegrid'):
+    plt.figure(figsize=(6, 4))
+    for lab, col in zip(('Iris-setosa', 'Iris-versicolor', 'Iris-virginica'),
+                        ('blue', 'red', 'green')):
+        plt.scatter(Y_sklearn[y==lab, 0],
+                    Y_sklearn[y==lab, 1],
+                    label=lab,
+                    c=col)
+    plt.xlabel('Componente Principale 1')
+    plt.ylabel('Componente Principale 2')
+    plt.legend(loc='lower center')
+    plt.tight_layout()
+    plt.show()
+
+if __name__ == '__main__':
+    pass
